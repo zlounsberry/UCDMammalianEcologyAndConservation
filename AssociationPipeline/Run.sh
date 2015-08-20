333189d558dd1218fd698ff17b9d1e6474b5b5cc
ZLounsberry <zlounsberry@gmail.com>
2015-08-20 12:53:24 -0700

    I updated a thing

--- AssociationPipeline/Run.sh
+++ AssociationPipeline/Run.sh
@@ -1,23 +1,16 @@
 #./bowtie.sh
 
-##################################################################
+############
+
 
 W=($(wc -l samplefiles))
-GENE=$(echo "IMPG2")
-#Change the word IMPG2 in the gene you want to look at. 
-
+GENE=$(echo "ENAM")
 #THRESHOLD=$(echo "0.005") #ignore this for now...
 
-#samplefiles is a 2-column, tab delimited file containing your sample IDs in column 1 and the disease state in column 2 (either the disease name or the word 'control'). Looks like this:
-	#G232	control
-	#G233-1	control
-	#G294-1	control
-	#G310-1	glaucoma
-	#G323	glaucoma
-	#G327	glaucoma
-	#G328	glaucoma
+#samplefiles is a 2-column, tab delimited file containing your sample IDs in column 1 and the disease state in column 2 (either the disease name or the word 'control')
+#Change the word COL8A in the variable above to the exact string that occurs in your samplefiles
 
-##################################################################
+###########
 
 rm bam.filelist
 mkdir output
@@ -51,13 +44,13 @@ done
 
 ./freebayes.sh
 
-##################################################################
+###################
 
-grep -e "#" -e "$GENE" ALL.vcf | /usr/bin/vcflib/bin/vcfsnps > ALL.vcf.$GENE
+grep -e "#" -e "$GENE" ALL.vcf > ALL.vcf.$GENE
 #Here is where your DISEASE variable from the beginning is used!
 #Careful, this method ignores INDELs! Will be trying to fix that soon...
 
-##################################################################
+###################
 
 /usr/bin/plink1.9/plink --vcf ALL.vcf.$GENE --allow-extra-chr --recode --noweb --out output/plink
 
@@ -78,7 +71,7 @@ do
 	c2=$2 #second variable (column 2 in files) This script reads lines one at a time.
 	c3=$3
 
-if grep -Fw "${c1}" samplefiles | grep -qFw "control" ; then
+if grep -Fw "${c1}" samplefiles | grep -iqFw "control" ; then
 	echo -e "${c1}\t${c1}\t1" >> output/pheno.txt
 else
 	echo -e "${c1}\t${c1}\t2" >> output/pheno.txt
@@ -90,40 +83,13 @@ done
 
 ./plink.sh
 
-./blacklist.sh
+###########################
 
-awk {'print $3'} output/output.assoc |  grep -v --file blacklisted.$GENE.snps > output/filtersites
-
-rm output/output.filtered.assoc
-
-Y=($(wc -l output/filtersites))
-
-x=1
-while [ $x -le $Y ] # "-le " refers to your sample size for the "while loop" (here: 96 samples which is the result of "wc -l files").
-do
-
-      string="sed -n ${x}p output/filtersites"
-
-        str=$($string)
-
-        var=$(echo $str | awk -F"\t" '{print $1, $2}')
-        set -- $var
-        c1=$1 #the first variable (column 1 in files)
-        c2=$2 #second variable (column 2 in files) This script reads lines one at a time.
-        c3=$3
-
-grep -Fw "${c1}" output/output.assoc >> output/output.filtered.assoc
-
-x=$(( $x + 1 ))
-
-done
-
-rm output/filtersites
-
-
-##################################################################
-
-awk '$9<0.01' output/output.filtered.assoc > output/output.filtered.cutoffPvalue.assoc
+awk '$9<0.005' output/output.assoc > output/output.filtered.cutoffPvalue.$GENE.assoc
+mv output/output.assoc output/output.$GENE.assoc
 #Change 0.001 to a value to pull P values less than that value. 
 
-##################################################################
+##########################
+
+mkdir $GENE
+mv output $GENE
